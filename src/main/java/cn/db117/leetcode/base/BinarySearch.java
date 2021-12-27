f7d4a9edf68332e9cd1066d9c3f8832148de4a27
db117 <z351622948@163.com>
2021-12-27 16:11:20 +0800

    BinarySearch 二分算法模板

--- src/main/java/cn/db117/leetcode/base/BinarySearch.java
+++ src/main/java/cn/db117/leetcode/base/BinarySearch.java
@@ -9,10 +9,14 @@ package cn.db117.leetcode.base;
 public class BinarySearch {
     public static void main(String[] args) {
         BinarySearch search = new BinarySearch();
-        System.out.println(search.bsRight(new int[]{
+        System.out.println(search.bsLeftMax(new int[]{
                         1, 2, 3, 4, 5, 9, 9, 9, 9, 9, 11, 15
                 },
-                9));
+                11));
+        System.out.println(search.bsLeftMax(new int[]{
+                        9, 9, 9, 9, 9
+                },
+                8));
     }
 
     /**
@@ -66,8 +70,28 @@ public class BinarySearch {
         return left;
     }
 
+    public int bsLeft1(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            // 左边中位数
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] < target) {
+                // 移动左边界
+                // 上面选择左边中位数,所有加一
+                left = mid + 1;
+            } else {
+                // 大于等于则保持右边界
+                // 等于则继续往左边查找
+                right = mid;
+            }
+        }
+
+        // 需要判断是否找到
+        return nums[right] == target ? right : -1;
+    }
+
     /**
-     * 查找最左边的索引,找不到返回-1
+     * 查找最右边的索引,找不到返回-1
      */
     public int bsRight(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
@@ -90,5 +114,66 @@ public class BinarySearch {
             return -1;
         }
         return right;
+    }
+
+    public int bsRight1(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            // 选右边中位数
+            int mid = left + ((right - left + 1) >> 1);
+            if (nums[mid] <= target) {
+                // 小于移动左边界
+                // 上面选择的是右边中位数,所以这里不加一
+                // 等于则继续往右边寻找
+                left = mid;
+            } else {
+                // 移动右边界
+                right = mid - 1;
+            }
+        }
+
+        // 需要判断是否找到
+        return nums[right] == target ? right : -1;
+    }
+
+    /**
+     * 找小于目标的最大值
+     */
+    public int bsLeftMax(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            // 选右边中位数
+            int mid = left + ((right - left + 1) >> 1);
+            if (nums[mid] < target) {
+                // 当前值可能是目标值,继续往右边找
+                left = mid;
+            } else {
+                // 当前值不可能是目标值,想左边找
+                right = mid - 1;
+            }
+        }
+        // 防止所有数据都大于目标值
+        return nums[right] < target ? right : -1;
+    }
+
+
+    /**
+     * 找大于目标的最小值
+     */
+    public int bsRightMin(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            // 选右边中位数
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] <= target) {
+                // 当前值不可能是目标,继续往右边找
+                left = mid + 1;
+            } else {
+                // 当前值可能是目标,继续往左边找
+                right = mid;
+            }
+        }
+        // 防止所有数据都小于目标值
+        return nums[right] > target ? right : -1;
     }
 }
