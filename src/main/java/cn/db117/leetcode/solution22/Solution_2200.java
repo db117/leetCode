65084dde0bae8224022d8c6dbeabf565f57dd710
db117 <z351622948@163.com>
2022-06-23 14:54:07 +0800

    2200.找出数组中的所有 K 近邻下标.find-all-k-distant-indices-in-an-array

--- src/main/java/cn/db117/leetcode/solution22/Solution_2200.java
+++ src/main/java/cn/db117/leetcode/solution22/Solution_2200.java
@@ -50,9 +50,7 @@
 package cn.db117.leetcode.solution22;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * 2200.找出数组中的所有 K 近邻下标.find-all-k-distant-indices-in-an-array
@@ -64,28 +62,45 @@ import java.util.Set;
 public class Solution_2200 {
     public static void main(String[] args) {
         Solution solution = new Solution_2200().new Solution();
+        System.out.println(solution.findKDistantIndices(new int[]{1}, 1, 1));
+        // [1,1000,1,1000]
+        //1
+        //1
+        System.out.println(solution.findKDistantIndices(new int[]{1, 1000, 1, 1000}, 1, 1));
+        // [734,228,636,204,552,732,686,461,973,874,90,537,939,986,855,387,344,939,552,389,116,93,545,805,572,306,157,
+        // 899,276,479,337,219,936,416,457,612,795,221,51,363,667,112,686,21,416,264,942,2,127,47,151,277,603,842,586,
+        // 630,508,147,866,434,973,216,656,413,504,360,990,228,22,368,660,945,99,685,28,725,673,545,918,733,158,254,207,
+        // 742,705,432,771,578,549,228,766,998,782,757,561,444,426,625,706,946]
+        //939
+        //34
+        System.out.println(solution.findKDistantIndices(new int[]{734, 228, 636, 204, 552, 732, 686, 461, 973, 874, 90, 537, 939, 986, 855, 387, 344, 939, 552, 389, 116, 93, 545, 805, 572, 306, 157,
+                899, 276, 479, 337, 219, 936, 416, 457, 612, 795, 221, 51, 363, 667, 112, 686, 21, 416, 264, 942, 2, 127, 47, 151, 277, 603, 842, 586,
+                630, 508, 147, 866, 434, 973, 216, 656, 413, 504, 360, 990, 228, 22, 368, 660, 945, 99, 685, 28, 725, 673, 545, 918, 733, 158, 254, 207,
+                742, 705, 432, 771, 578, 549, 228, 766, 998, 782, 757, 561, 444, 426, 625, 706, 946}, 939, 34));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+            List<Integer> ans = new ArrayList<>();
 
-            Set<Integer> set = new HashSet<>();
             int i = 0;
+            int next = 0;// 下一个可以添加的数字
             while (i < nums.length) {
                 if (nums[i] == key) {
                     // 把 k 之内的数字全加上
-                    int left = Math.max(0, i - k);
-                    int right = Math.max(nums.length - 1, i + k);
+                    int left = Math.max(next, i - k);
+                    int right = Math.min(nums.length - 1, i + k);
                     for (int j = left; j <= right; j++) {
-                        set.add(j);
+                        ans.add(j);
                     }
-                    i = right + 1;
+                    next = right + 1;
                 }
                 i++;
+
             }
-            List<Integer> ans = new ArrayList<>(set);
-            ans.sort(Integer::compareTo);
+
+
             return ans;
         }
     }
